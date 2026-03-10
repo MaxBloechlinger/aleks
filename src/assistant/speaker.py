2223d9596516eb3d01b350e6b599b9a7fffcc187
MaxBloechlinger <max.bloechlinger.job@gmail.com>
2026-03-10 17:35:42 +0100

    requirements and listener update

--- src/assistant/speaker.py
+++ src/assistant/speaker.py
@@ -1,2 +1,12 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+
+engine.setProperty("rate", 170)
+engine.setProperty("volume", 1.0)
+
+
 def speak(text):
     print("Aleks:", text)
+    engine.say(text)
+    engine.runAndWait()
