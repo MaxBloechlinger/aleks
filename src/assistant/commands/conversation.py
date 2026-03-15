b060837b09a4711029500d725b4ea0789bc8e7e7
MaxBloechlinger <max.bloechlinger.job@gmail.com>
2026-03-15 15:51:02 +0100

    added morning command

--- src/assistant/commands/conversation.py
+++ src/assistant/commands/conversation.py
@@ -4,6 +4,9 @@ from .router import register
 def hello(text):
     return "Hello Sir"
 
+def good_morning(text):
+    return "Good Morning Me Lord. May your highness have a splendid Day."
+
 
 def who(text):
     return "I'm Aleks, your assistant."
@@ -14,5 +17,6 @@ def thanks(text):
 
 
 register(["hello"], hello)
+register(["good morning"], good_morning)
 register(["who"], who)
 register(["thanks", "thank you"], thanks)
