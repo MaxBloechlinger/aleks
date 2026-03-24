72a0c84b567c421473276062698eac673ccf5374
MaxBloechlinger <max.bloechlinger.job@gmail.com>
2026-03-24 20:56:50 +0100

    added more stable version

--- src/assistant/speaker.py
+++ src/assistant/speaker.py
@@ -1,11 +1,10 @@
-import sounddevice as sd
-import soundfile as sf
+import subprocess
+import tempfile
 import time
 import threading
+import wave
 from pathlib import Path
 from piper.voice import PiperVoice
-import tempfile
-import wave
 
 speaking_event = threading.Event()
 last_spoken_time = 0
@@ -27,19 +26,18 @@ def speak(text):
 
     with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as f:
 
-        # create proper WAV writer
         with wave.open(f.name, "wb") as wav_file:
             voice.synthesize_wav(text, wav_file)
 
-        audio, sample_rate = sf.read(f.name)
-
-        sd.play(audio, sample_rate)
-        sd.wait()
+        subprocess.run(
+            ["aplay", "-q", f.name],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
 
     last_spoken_time = time.time()
     speaking_event.clear()
 
 
 def stop():
-    sd.stop()
     speaking_event.clear()
